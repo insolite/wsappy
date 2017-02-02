27a51850fc99bf32871fe924615049e3c0061021
cetus <a.insolite@gmail.com>
2017-02-02 15:32:13 +0200

    Add license file, refactor setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,26 @@
 from setuptools import setup
 from pip.req import parse_requirements
 
+import wsappy as package
+
 
 install_reqs = parse_requirements('requirements.txt', session=False)
 requirements = [str(ir.req) for ir in install_reqs]
 
 setup(
-    name='wsappy',
-    version='0.1.0',
+    name=package.__name__,
+    version=package.__version__,
+    packages=[package.__name__],
+    url='https://github.com/insolite/{}'.format(package.__name__),
+    author='Oleg Krasnikov',
+    author_email='a.insolite@gmail.com',
     description='A bit more higher level of asyncio websocket server',
+    include_package_data=True,
     classifiers=[
         'Intended Audience :: Developers',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5'],
-    author='Oleg Krasnikov',
-    author_email='a.insolite@gmail.com',
-    url='https://github.com/insolite/wsappy',
-    download_url='https://github.com/insolite/wsappy',
-    packages=['wsappy'],
-    include_package_data=True,
     install_requires=requirements,
 )
