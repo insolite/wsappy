74720c6c31777ce60d4cc5cf7de11ecf3d5570dc
Oleg Krasnikov <a.insolite@gmail.com>
2017-02-03 23:44:17 +0200

    Fix setuptools configuration

--- setup.py
+++ setup.py
@@ -1,17 +1,32 @@
+import re
+import os
 from setuptools import setup
 from pip.req import parse_requirements
-
-import wsappy as package
 
 
 install_reqs = parse_requirements('requirements.txt', session=False)
 requirements = [str(ir.req) for ir in install_reqs]
+package_name = 'wsappy'
+hyphen_package_name = package_name.replace('_', '-')
+
+
+def read_version():
+    regexp = re.compile(r"^__version__\s*=\s*'([\d.abrc]+)'")
+    init_py = os.path.join(os.path.dirname(__file__), package_name, '__init__.py')
+    with open(init_py) as f:
+        for line in f:
+            match = regexp.match(line)
+            if match is not None:
+                return match.group(1)
+        else:
+            raise RuntimeError('Cannot find version in {}'.format(init_py))
+
 
 setup(
-    name=package.__name__,
-    version=package.__version__,
-    packages=[package.__name__],
-    url='https://github.com/insolite/{}'.format(package.__name__),
+    name=hyphen_package_name,
+    version=read_version(),
+    packages=[package_name],
+    url='https://github.com/insolite/{}'.format(hyphen_package_name),
     author='Oleg Krasnikov',
     author_email='a.insolite@gmail.com',
     description='A bit more higher level of asyncio websocket server',
